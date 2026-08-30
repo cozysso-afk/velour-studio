7bb7f4b8ebbb9cb85b5b30ef4d4ed98fda1a43e0
cozysso-afk <cozysso@gmail.com>
2026-08-30 16:55:14 +0900

    chore: load continuity vault edge fix

--- velour-v4.4.38-vault-accept-hotfix.js
+++ velour-v4.4.38-vault-accept-hotfix.js
@@ -219,3 +219,12 @@
   script.async = false;
   document.head.appendChild(script);
 })();
+
+(() => {
+  if (window.__VELOUR_CONTINUITY_VAULT_EDGE_LOADER__) return;
+  window.__VELOUR_CONTINUITY_VAULT_EDGE_LOADER__ = true;
+  const script = document.createElement('script');
+  script.src = './velour-v4.4.38-continuity-vault-hotfix.js?v=1';
+  script.async = false;
+  document.head.appendChild(script);
+})();
