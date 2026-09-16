50832f49ef73550e8d6deedc0059f2cb01d63295
cozysso-afk <cozysso@gmail.com>
2026-09-16 10:56:43 +0900

    Load final HARD CANON lock after prompt wrappers

--- velour-v4.4.38-vault-accept-hotfix.js
+++ velour-v4.4.38-vault-accept-hotfix.js
@@ -33,3 +33,4 @@ __velourLoadHotfix('__VELOUR_INTERNAL_LABEL_FIREWALL_LOADER__','./velour-v4.4.38
 __velourLoadHotfix('__VELOUR_PROSE_QA_LOADER__','./velour-v4.4.38-prose-qa-hotfix.js?v=1');
 __velourLoadHotfix('__VELOUR_STYLE_DNA_LOADER__','./velour-v4.4.38-style-dna-hotfix.js?v=3');
 __velourLoadHotfix('__VELOUR_SCENE_VOICE_MEMORY_LOADER__','./velour-v4.4.38-scene-voice-memory-hotfix.js?v=1');
+__velourLoadHotfix('__VELOUR_HARD_CANON_LOCK_LOADER__','./velour-v4.4.38-hard-canon-lock-hotfix.js?v=1');
