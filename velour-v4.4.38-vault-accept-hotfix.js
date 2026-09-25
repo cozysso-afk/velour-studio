3c3a96b42af128b5c91964a982fbe5c25fd7fc42
cozysso-afk <cozysso@gmail.com>
2026-09-25 06:39:03 -0700

    Bust language firewall module cache

--- velour-v4.4.38-vault-accept-hotfix.js
+++ velour-v4.4.38-vault-accept-hotfix.js
@@ -37,5 +37,5 @@ __velourLoadHotfix('__VELOUR_HARD_CANON_LOCK_LOADER__','./velour-v4.4.38-hard-ca
 __velourLoadHotfix('__VELOUR_CONTEXTUAL_DIALOGUE_LOADER__','./velour-v4.4.38-contextual-dialogue-engine.js?v=3');
 __velourLoadHotfix('__VELOUR_CONCEPT_RELATIONSHIP_GOVERNOR_LOADER__','./velour-v4.4.38-concept-relationship-governor.js?v=2');
 __velourLoadHotfix('__VELOUR_VERBAL_CHEMISTRY_V2_LOADER__','./velour-v4.4.38-verbal-chemistry-v2.js?v=2');
-__velourLoadHotfix('__VELOUR_VERBAL_CHEMISTRY_REFINEMENT_LOADER__','./velour-v4.4.38-verbal-chemistry-refinement.js?v=2');
+__velourLoadHotfix('__VELOUR_VERBAL_CHEMISTRY_REFINEMENT_LOADER__','./velour-v4.4.38-verbal-chemistry-refinement.js?v=3');
 __velourLoadHotfix('__VELOUR_VAULT_LANGUAGE_FIREWALL_LOADER__','./velour-v4.4.38-vault-language-firewall.js?v=1');
